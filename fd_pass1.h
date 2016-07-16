063e41177cc6aa4283a3cea352d399d7cfe411c2
muxa <mikael-1994@ya.ru>
2016-07-16 20:50:55 +0300

    cleanup

--- fd_pass1.h
+++
@@ -1,8 +0,0 @@
-int
-send_file_descriptor(
-     int socket, /* Socket through which the file descriptor is passed */
-     int fd_to_send);/* File descriptor to be passed, could be another socket */
-
-int
-recv_file_descriptor(
-     int socket); /* Socket from which the file descriptor is read */
